3279a353862440ca75ded631c18a680685fac4af
expool <gosha.shcherbak@mail.ru>
2025-09-27 13:32:01 +0300

    Create build_app.bat

---
+++ build_app.bat
@@ -0,0 +1,7 @@
+@echo off
+cd "path/to/project_folder"
+call .qtcreator/Scripts/activate
+echo Building started
+pyinstaller --clean --onefile --icon=icon.ico --windowed --add-binary ".\ffmpeg.exe;." --add-data "src/img/no_connection.png;img" --add-data "src/img/connection_established.png;img" --add-data "resources/lang/ru.json;resources/lang" --add-data "resources/lang/en.json;resources/lang" --hidden-import 'zeroconf._utils.ipaddress' --hidden-import 'zeroconf._handlers.answers' -w main.py
+rename .\dist\main.exe client_with_new_ffmpeg.exe
+echo Build was successful
