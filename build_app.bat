7eb4a70d15c109677cd28f46aa1a76e8da032de0
expool <gosha.shcherbak@mail.ru>
2025-09-29 17:13:21 +0300

    Update build_app.bat

--- build_app.bat
+++ build_app.bat
@@ -1,5 +1,4 @@
 @echo off
-cd "path/to/project_folder"
 call .qtcreator/Scripts/activate
 echo Building started
 pyinstaller --clean --onefile --icon=icon.ico --windowed --add-binary ".\ffmpeg.exe;." --add-data "src/img/no_connection.png;img" --add-data "src/img/connection_established.png;img" --add-data "resources/lang/ru.json;resources/lang" --add-data "resources/lang/en.json;resources/lang" --hidden-import 'zeroconf._utils.ipaddress' --hidden-import 'zeroconf._handlers.answers' -w main.py
